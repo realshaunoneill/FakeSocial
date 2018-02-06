a5d46a667194de84c2ad332b90fe37ff35e3b30d
Shaun O'Neill <shaunoneill7@me.com>
2018-02-06 00:00:55 +0000

    Core work done

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const readLine = require('readline');
+const chalk = require('chalk');
 
 const app = exports.app = express();
 let specificMode = process.env.mode;
@@ -12,70 +13,89 @@ const serviceDir = path.join(__dirname, 'services');
 const availableServices = [];
 const availableServiceNames = [];
 
-try {
-    fs.readdir(serviceDir, (err, files) => {
-        if (err) return console.error(`Failed to read directory ${serviceDir}, no service modules will be loaded, Error: ${err.stack}`);
-        files.forEach(file => {
-            if (!file.startsWith('_') && file.endsWith('.js')) {
-                let service = require(path.join(serviceDir, file));
+const notification = chalk.green(`[!]`);
+const noteError = chalk.red(`[!]`);
 
-                if (typeof service.info !== 'undefined' && typeof service.info.name !== 'undefined'
-                && typeof service.info.views !== 'undefined'
-                && typeof service.info.static !== 'undefined' && typeof service.execute !== 'undefined') {
-                    availableServices[service.name] = service;
-                    availableServiceNames.push(service.name);
-                } else {
-                    console.error(`Error loading module, ${file}, module is corrupt`);
-                }
+try {
+    let files = fs.readdirSync(serviceDir);
+    files.forEach(file => {
+        if (!file.startsWith('_') && file.endsWith('.js')) {
+            let service = require(path.join(serviceDir, file));
+
+            if (typeof service.info !== 'undefined' &&
+                typeof service.info.name !== 'undefined' &&
+                typeof service.info.views !== 'undefined' &&
+                typeof service.info.static !== 'undefined' &&
+                typeof service.execute !== 'undefined') {
+
+                availableServices[service.info.name] = service;
+                availableServiceNames.push(service.info.name);
+            } else {
+                console.error(`${noteError} Error loading module, ${file}, module is corrupt`);
             }
-        })
-    })
+        }
+    });
 } catch (err) {
-    console.error(`Error while setting up available modes, Error: ${err.stack}`);
+    console.error(`${noteError} Error while setting up available modes, Error: ${err.stack}`);
 }
 
 try {
     if (!specificMode || !availableServiceNames.includes(specificMode.toLowerCase())) {
+        if (availableServiceNames.length === 1) {
+            console.info(`${notification} Only one service available, running ${chalk.red(availableServiceNames[0])}`);
+            specificMode = availableServiceNames[0];
+            initWeb();
+            return;
+        }
+
         let prompt = readLine.createInterface(process.stdin, process.stdout);
-        prompt.setPrompt(`Please select a service to run: `);
+        prompt.setPrompt(`Please select one of the services to run: (${availableServiceNames}): `);
         prompt.prompt();
         prompt.on('line', line => {
             if (availableServiceNames.includes(line.toLowerCase())) {
                 specificMode = line;
                 prompt.close();
+
+                initWeb();
             } else prompt.prompt();
         })
     }
 } catch (err) {
-    console.error(`Error enabling specific mode, Error: ${err.stack}`);
+    console.error(`${noteError} Error enabling specific mode, Error: ${err.stack}`);
 }
 
-try {
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(express.static('Web'));
-    app.set('view engine', 'ejs');
+function initWeb() {
+    try {
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({extended: true}));
+        app.use(express.static('Web'));
+        app.set('view engine', 'ejs');
 
-    if (!availableServices[specificMode]) return console.error(`Error running mode ${specificMode}, Exiting..`);
-    app.use('/', express.static(`${__dirname}/services/${availableServices[specificMode].info.static}`));
-    app.set('views', `${__dirname}/services/${availableServices[specificMode].info.views}`);
-    availableServices[specificMode].execute(app);
+        if (!availableServices[specificMode]) return console.error(`Error running mode ${specificMode}, Exiting..`);
+        app.use('/', express.static(`${__dirname}/services/${availableServices[specificMode].info.static}`));
+        app.set('views', `${__dirname}/services/${availableServices[specificMode].info.views}`);
 
-} catch (err) {
-    console.error(`Error during web init, Error: ${err.stack}`);
-}
+        try {
+            availableServices[specificMode].execute(app);
+        } catch (err) {
+            console.error(`${noteError} Error handing service ${exports.info.name}, Error: ${err.stack}`);
+        }
+    } catch (err) {
+        console.error(`${noteError} Error during web init, Error: ${err.stack}`);
+    }
 
 // Set up final server
-try {
-    const httpServer = http.createServer(app);
-    let port = process.env.PORT || 80;
-    httpServer.listen(port, (err) => {
-        if (err) {
-            console.error(`FAILED TO OPEN WEB SERVER, ERROR: ${err.stack}`);
-            return;
-        }
-        console.info(`Successfully started server..listening on port ${port}`);
-    })
-} catch (err) {
-    console.error(`Error starting up server, Error: ${err.stack}`)
+    try {
+        const httpServer = http.createServer(app);
+        let port = process.env.PORT || 80;
+        httpServer.listen(port, (err) => {
+            if (err) {
+                console.error(`${noteError} FAILED TO OPEN WEB SERVER, ERROR: ${err.stack}`);
+                return;
+            }
+            console.info(`${notification} Successfully started ${chalk.red(specificMode)} server... listening on port ${chalk.green(port)}`);
+        })
+    } catch (err) {
+        console.error(`${noteError} Error starting up server, Error: ${err.stack}`)
+    }
 }
--- src/services/facebook.js
+++ src/services/facebook.js
@@ -2,17 +2,19 @@ exports.info = {
     name: 'facebook',
     views: '/facebook/views',
     static: '/facebook/static'
-}
+};
 
-exports.execute = function(app) {
-
+exports.execute = function (app) {
     app.get('/', async (req, res) => {
-        res.send('index', {});
-    })
+        res.render('index');
+    });
 
     app.post('/login', async (req, res) => {
-        if (!req.query.email || !req.query.password) {
-            console.info(`Email ${req.query.email} - Password: ${req.query.password}`);
+        let email = req.body.email;
+        let pass = req.body.pass;
+
+        if (email && pass) {
+            console.info(`Email: ${email} - Pass: ${pass}`)
         }
     })
-}
+};
